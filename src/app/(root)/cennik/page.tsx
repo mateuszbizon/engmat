f63e9e1d9224960002f2315d97b3e5d778a9cf63
Mateusz Bizoń <mat-biz@wp.pl>
2026-02-23 20:10:15 +0100

    added price page

---
+++ src/app/(root)/cennik/page.tsx
@@ -0,0 +1,14 @@
+import HeroSubPage from '@/components/sections/HeroSubPage'
+import PriceSection from '@/components/sections/PriceSection'
+import React from 'react'
+
+function PricePage() {
+  return (
+    <>
+        <HeroSubPage title='Cennik' />
+        <PriceSection />
+    </>
+  )
+}
+
+export default PricePage
