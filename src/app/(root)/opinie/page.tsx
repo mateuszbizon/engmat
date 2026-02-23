391b729a5aed275c6ba875e51b64fc5d768fc9fe
Mateusz Bizoń <mat-biz@wp.pl>
2026-02-23 19:48:53 +0100

    added reviews page

---
+++ src/app/(root)/opinie/page.tsx
@@ -0,0 +1,14 @@
+import HeroSubPage from '@/components/sections/HeroSubPage'
+import ReviewsSection from '@/components/sections/ReviewsSection'
+import React from 'react'
+
+function ReviewsPage() {
+  return (
+    <>
+        <HeroSubPage title='Opinie' />
+        <ReviewsSection />
+    </>
+  )
+}
+
+export default ReviewsPage
