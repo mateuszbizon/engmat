6e04c2cdbf88a0cc7496ce8c76a3011d3b5d542f
Mateusz Bizoń <mat-biz@wp.pl>
2026-02-25 11:30:32 +0100

    changed numbers

--- src/components/sections/NumbersSection.tsx
+++ src/components/sections/NumbersSection.tsx
@@ -12,16 +12,16 @@ function NumbersSection() {
         <Container>
             <div className='grid lg:grid-cols-3 gap-x-5 gap-y-10'>
                 <div className='flex flex-col gap-5'>
-                    <span className='heading3'>+<CountUp from={0} to={200} delay={0.25} /></span>
-                    <span className='bigger-text'>zadowolonych klientów</span>
-                </div>
-                <div className='flex flex-col gap-5'>
                     <span className='heading3'>+<CountUp from={0} to={500} delay={0.25} /></span>
                     <span className='bigger-text'>zadowolonych klientów</span>
                 </div>
                 <div className='flex flex-col gap-5'>
-                    <span className='heading3'>+<CountUp from={0} to={351} delay={0.25} /></span>
-                    <span className='bigger-text'>zadowolonych klientów</span>
+                    <span className='heading3'>+<CountUp from={0} to={20} delay={0.25} /></span>
+                    <span className='bigger-text'>lat doświadczenia</span>
+                </div>
+                <div className='flex flex-col gap-5'>
+                    <span className='heading3'>+<CountUp from={0} to={200} delay={0.25} /></span>
+                    <span className='bigger-text'>uczniów rocznie</span>
                 </div>
             </div>
         </Container>
