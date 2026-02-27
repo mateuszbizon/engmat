489a93350c9eb7b1ed68078d13d5cb7816b53757
Mateusz Bizoń <mat-biz@wp.pl>
2026-02-27 19:59:47 +0100

    moved getting all blogs to another function

--- src/components/blog/AllBlogs.tsx
+++ src/components/blog/AllBlogs.tsx
@@ -1,18 +1,13 @@
-import { client } from '@/sanity/lib/client'
-import { GET_ALL_BLOGS_QUERY } from '@/sanity/lib/queries'
 import React from 'react'
 import BlogCard from '../cards/BlogCard'
+import { getAllPosts } from '@/sanity/posts/getAllPosts'
 
 async function AllBlogs() {
-    const blogs = await client.withConfig({
-        useCdn: false
-    }).fetch(GET_ALL_BLOGS_QUERY, {}, {
-        next: { revalidate: 60 }
-    })
+    const { posts, success, message } = await getAllPosts()
 
   return (
     <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10'>
-        {blogs.map(item => (
+        {posts.map(item => (
             <BlogCard key={item._id} blog={item} />
         ))}
     </div>
---
+++ src/sanity/posts/getAllPosts.ts
@@ -0,0 +1,26 @@
+import { client } from "../lib/client";
+import { GET_ALL_BLOGS_QUERY } from "../lib/queries";
+
+export async function getAllPosts() {
+    try {
+        const posts = await client.withConfig({
+            useCdn: false
+        }).fetch(GET_ALL_BLOGS_QUERY, {}, {
+            next: { revalidate: 60 }
+        })
+
+        return {
+            posts: posts || [],
+            success: true,
+            message: ""
+        }
+    } catch (error) {
+        console.error("Błąd podczas pobierania wszystkich postów")
+        
+        return {
+            posts: [],
+            success: false,
+            message: "Błąd podczas pobierania wszystkich postów"
+        }
+    }
+}
