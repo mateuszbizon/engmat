77e68b132045f5d6239ca9146c5d2f1547766a41
Mateusz Bizoń <mat-biz@wp.pl>
2026-02-27 14:55:28 +0100

    added sanity types

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { categoryType } from './categoryType'
+import { postType } from './postType'
+import { blockContentType } from './blockContentType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [categoryType, postType, blockContentType],
 }
