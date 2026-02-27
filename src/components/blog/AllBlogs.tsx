29bbbe52757fd967a97cdb4a3ddcd9d9e635c802
Mateusz Bizoń <mat-biz@wp.pl>
2026-02-27 20:12:34 +0100

    added messages for error and empty

--- src/components/blog/AllBlogs.tsx
+++ src/components/blog/AllBlogs.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import BlogCard from '../cards/BlogCard'
 import { getAllPosts } from '@/sanity/posts/getAllPosts'
+import ErrorMessage from '../messages/ErrorMessage'
 
 async function AllBlogs() {
     const { posts, success, message } = await getAllPosts()
+
+    if (!success) return <ErrorMessage description={message} />
 
   return (
     <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-10'>
